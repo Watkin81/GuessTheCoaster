12edbcf79f4857510a51740c154b18ca5eb66e0e
Jake Watkins <76190146+Watkin81@users.noreply.github.com>
2023-11-17 15:08:54 -0500

    Add files via upload

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -4,6 +4,18 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`Ready!!! ${client.user.tag} is logged in and online!`);
+        console.log(`Currently in ${client.guilds.cache.size} servers!`);
+
+        const games = ["Planet Coaster", "NoLimits 2", "OpenRCT2"];
+        let counter = 0;
+
         client.user.setActivity({name: "OpenRCT2", type: ActivityType.Playing});
+        setInterval(() => {
+            client.user.setActivity({name: games[counter % games.length], type: ActivityType.Playing});
+            counter++;
+            if (counter === games.length) {
+                counter = 0;
+            };
+        }, 1200000);
     }
 }
